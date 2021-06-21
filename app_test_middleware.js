d64bfcc8983624e093adfb07b09997c37c968ced
Nguyen The Nhat <nhat0103st@gmail.com>
2021-06-21 09:07:10 +0700

    using middleware - 5 types

--- app_test_middleware.js
+++ app_test_middleware.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+let router = express.Router();
 
 // //-- this is a middleware function
 // let logger = function (req, res, next) {
@@ -29,4 +30,68 @@ let app = express();
 // }))
 //
 
+/** Application-level middleware */
+
+//-- without path
+// app.use(function (req, res, next) {
+//     console.log('This middleware function run before any request');
+//     next();
+// })
+//
+// //-- with specified path
+// app.use('/user/:id', function (req, res, next) {
+//     console.log('This middleware function run before user/' + req.params.id);
+//     next();
+// })
+//
+// app.get('/user/:id', (req, res, next) => {
+//     res.send('Hello');
+// })
+
+//-- To skip rest of next function - use next('route')
+// app.get('/user/:id', function (req, res, next) {
+//     // if the user ID is 0, skip to the next route
+//     if (req.params.id === '0') next('route')
+//     // otherwise pass the control to the next middleware function in this stack
+//     else next()
+// }, function (req, res, next) {
+//     // send a regular response
+//     res.send('regular')
+// })
+
+//-- array of middleware functions
+// function logOriginalUrl (req, res, next) {
+//     console.log('Request URL:', req.originalUrl)
+//     next()
+// }
+//
+// function logMethod (req, res, next) {
+//     console.log('Request Type:', req.method)
+//     next()
+// }
+//
+// let  logStuff = [logOriginalUrl, logMethod];
+//
+// app.get('/user/:id', logStuff, function (req, res, next) {
+//     res.send('User Info')
+// })
+
+/** Router-level middleware
+ * Just like application-level
+ * Used with router object
+ *  */
+
+/** Error-handling middleware
+ * Must have 4 parameter (err, req, res, next)
+ */
+
+/** Built-in middleware
+ * express.static serves static assets such as HTML files, images, and so on.
+ * express.json parses incoming requests with JSON payloads
+ * express.urlencoded parses incoming requests with URL-encoded payloads
+ */
+
+/** Third-party middleware
+ * https://expressjs.com/en/resources/middleware.html
+ */
 app.listen(3000)
